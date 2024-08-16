a387d8f5da7caff0bb3329e02894da10b7fc7946
Henrique Souza <henriquetsi10@gmail.com>
2024-08-16 11:51:47 -0300

    fix: fix path dbjson

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
+const fs = require('fs');
+const path = require('path')
 const jsonServer = require('json-server');
 
 const server = jsonServer.create()
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares)
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
   '/api/*': '/$1',
   '/blog/:resource/:id/show': '/:resource/:id'
-}))
+}));
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
-})
+});
 
 // Export the Server API
 module.exports = server
